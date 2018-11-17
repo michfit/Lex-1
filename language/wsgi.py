9e4dc1d89fa82d0b172071a9daf82112248796d8
LanguageExchange <44514024+LanguageExchange@users.noreply.github.com>
2018-11-16 17:58:11 -0800

    Update wsgi.py

--- language/wsgi.py
+++ language/wsgi.py
@@ -10,9 +10,6 @@ https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-import os
-
-from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'language.settings')
 
